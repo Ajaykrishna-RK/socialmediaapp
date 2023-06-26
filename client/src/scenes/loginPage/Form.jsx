f53e951b99ed5b40d7991b55191ec170234e361d
Ajay <ajayajuu73@gmail.com>
2023-06-26 17:09:15 +0530

    new commmit

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -68,7 +68,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "https://socialmediaapp-b9e7.onrender.com/auth/register",
+      "http://localhost:3001/auth/register",
       {
         method: "POST",
         body: formData,
@@ -85,7 +85,7 @@ const Form = () => {
 
   const login = async (values, onSubmitProps) => {
     try{
-      const loggedInResponse = await fetch("https://socialmediaapp-b9e7.onrender.com/auth/login", {
+      const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(values),
